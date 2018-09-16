5bf5df4f35542ed0ecb395d5f2f57daadc064b98
Tran Vu Hao <theflash8888@gmail.com>
2018-09-16 22:19:38 +0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
+var include = require('gulp-file-include');
+
+gulp.task('sass', function () {
+	return gulp.src('assets/scss/style.scss')
+	  .pipe(sass())
+	  .pipe(gulp.dest('assets/css'))
+	  .pipe(livereload());
+});
+
+gulp.task('include', function() {
+  return gulp.src(['html/*.html'])
+    .pipe(include({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('./'))
+    .pipe(livereload());
+});
+
+gulp.task('watch', function () {
+	livereload.listen();
+  gulp.watch(['./assets/scss/**/*'], ['sass']);
+  gulp.watch(['**/*.html'], ['include']);
+});
+
+gulp.task('server', function() {
+  browserSync({
+    notify: false,
+    server: {
+      baseDir: '.'
+    }
+  });
+  gulp.watch('./*.html', reload);
+  gulp.watch('./assets/css/*.css', reload);
+  gulp.watch('./assets/js/*.js', reload);
+});
+
+gulp.task('default', ['include', 'server', 'sass', 'watch']);
