25db7f85487ba67112d681cd2a4f4215754411ed
Tran Vu Hao <theflash8888@gmail.com>
2018-09-18 01:33:23 +0700

    make header, footer

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+;(function($){
+  var $btnMenu = null,
+      $nav = null;
+  function init() {
+    $btnMenu = $(".btn_menu");
+    $nav = $("nav");
+
+    $btnMenu.on("click", (e) => {
+      e.preventDefault();
+      $nav.stop().slideToggle();
+    });
+  }
+  $(function() {
+    init();
+  });
+})(jQuery);
