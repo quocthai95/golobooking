e3d752196ad7b0b5b38374faafa0928a83bc44fc
Tran Vu Hao <theflash8888@gmail.com>
2018-09-28 00:55:10 +0700

    update search supplier

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,9 @@
     $title = null,
     $btnQuote = null,
     $empty = null,
-    $resultQuote = null;
+    $resultQuote = null,
+    status = false;
+
 
   function init() {
     $btnMenu = $(".btn_menu");
@@ -14,6 +16,7 @@
     $btnQuote = $("#get_quote");
     $empty = $(".empty_value");
     $resultQuote = $(".quote_result");
+    $parent = $(".choose_area");
 
     $resultQuote.hide();
 
@@ -29,6 +32,21 @@
       $resultQuote.show();
     });
 
+
+    disableForm(".btn_checkbox", ".form_inner", ".form_wrap");
+    disableForm(".small_checkbox", ".small_inner", ".small_wrap");
+
+  }
+
+  function disableForm(ele, inner, wrap) {
+    $(ele).click(function() {
+      if ( this.checked ) {
+        $(inner).removeClass("active");
+        $(this).parents(wrap).find(inner).addClass("active");
+      } else {
+        $(this).parents(wrap).find(inner).removeClass("active");
+      }
+    });
   }
   
   function setWidth() {
